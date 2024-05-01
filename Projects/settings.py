6fcb11000f7d955ec944d15ccbe4d8db89cc531a
Mohit Pandey <mp7698921@gmail.com>
2024-05-01 22:12:43 +0530

    vercel host

--- Projects/settings.py
+++ Projects/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'Projects.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates'],
+        'DIRS': [BASE_DIR,'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
